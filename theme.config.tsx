a301a7a4c17d6eb8e53c40ccb79e3e70371483a1
Agustin Kassis <webmaster@masize.com>
2023-07-19 16:18:21 -0300

    updated theme.config

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,6 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>La Crypta Wallet</span>,
-  project: {
-    // link: "https://github.com/shuding/nextra-docs-template",
-  },
   chat: {
     link: "https://discord.lacrypta.ar",
   },
