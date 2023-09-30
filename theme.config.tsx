213ce60b93d76e6457d02950ae8aaa7681f51601
Agustin Kassis <webmaster@masize.com>
2023-09-30 20:23:18 -0300

    added footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import Logo from "./components/Logo";
+import LaCryptaLogo from "./components/LaCryptaLogo";
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
@@ -9,7 +10,16 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/lacrypta/wallet-docs",
   footer: {
-    text: "La Crypta Docs",
+    text: (
+      <div style={{ textAlign: "center", margin: "auto" }}>
+        <div>powered by </div>
+        <div style={{ marginTop: "8px" }}>
+          <a target='_blank' href='https://lacrypta.ar'>
+            <LaCryptaLogo />
+          </a>
+        </div>
+      </div>
+    ),
   },
 };
 
